fea32b9fac31dc4ecee7f9acfaa5a7cc57ed1619
Subrat Meher <smeher02.aoe3@gmail.com>
2018-09-14 21:49:26 +0200

    Add emacs config

---
+++ emacs/.emacs.d/elpa/dante-20180819.1435/dante-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dante" "20180819.1435" "Development mode for Haskell" '((dash "2.12.0") (emacs "25.1") (f "0.19.0") (flycheck "0.30") (haskell-mode "13.14") (s "1.11.0") (lcr "1.0")) :commit "8b3e79971b57b68a99336aca06650cbddc85816f" :keywords '("haskell" "tools") :authors '(("Jean-Philippe Bernardy" . "jeanphilippe.bernardy@gmail.com")) :maintainer '("Jean-Philippe Bernardy" . "jeanphilippe.bernardy@gmail.com") :url "https://github.com/jyp/dante")
---
+++ emacs/.emacs.d/elpa/swiper-20180813.1625/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20180813.1625" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "65f865853b3bffba287b70208fb6d7f6e833f3cc" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
