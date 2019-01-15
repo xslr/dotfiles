7b09b89a4b18388acbf63958cb06391b55c1cfa9
Subrat Meher <smeher02.aoe3@gmail.com>
2019-01-15 21:50:10 +0100

    emacs: remove packages downloaded from repo

--- emacs/.emacs.d/elpa/projectile-ripgrep-0.4.0/projectile-ripgrep-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile-ripgrep" "0.4.0" "Run ripgrep with Projectile" '((ripgrep "0.3.0") (projectile "0.14.0")) :commit "73595f1364f2117db49e1e4a49290bd6d430e345" :keywords '("ripgrep" "projectile") :authors '(("Nicolas Lamirault" . "nicolas.lamirault@gmail.com")) :maintainer '("Nicolas Lamirault" . "nicolas.lamirault@gmail.com") :url "https://github.com/nlamirault/ripgrep.el")
--- emacs/.emacs.d/elpa/ripgrep-20180323.1620/ripgrep-autoloads.el
+++
@@ -1,41 +0,0 @@
-;;; ripgrep-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "ripgrep" "../../../../../.emacs.d/elpa/ripgrep-20180323.1620/ripgrep.el"
-;;;;;;  "ed795249e4d9aa4fd28a4276feb7bf2b")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/ripgrep-20180323.1620/ripgrep.el
-
-(autoload 'ripgrep-regexp "ripgrep" "\
-Run a ripgrep search with `REGEXP' rooted at `DIRECTORY'.
-`ARGS' provides Ripgrep command line arguments.
-
-\(fn REGEXP DIRECTORY &optional ARGS)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "ripgrep" "../../../../../.emacs.d/elpa/ripgrep-20180323.1620/ripgrep.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/ripgrep-20180323.1620/ripgrep.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ripgrep" '("ripgrep")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/ripgrep-20180323.1620/ripgrep-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/ripgrep-20180323.1620/ripgrep.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ripgrep-autoloads.el ends here
